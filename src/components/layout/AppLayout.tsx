d75b8c1f6b6dcfb8987ed600e1255aafd8032a77
kevinveigaa <92695662+kevinveigaa@users.noreply.github.com>
2026-08-10 22:12:44 -0300

    feat: add ScrollToTop, hide navbar/footer on player page

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,19 +1,20 @@
-﻿import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import { ScrollToTop } from './ScrollToTop';
 
 export function AppLayout() {
+  const location = useLocation();
+  const isPlayer = location.pathname.startsWith('/assistir');
+
   return (
     <div className="flex min-h-screen flex-col bg-ink-950">
-      <Navbar />
-      <main className="flex-1 pt-16">
+      {!isPlayer && <Navbar />}
+      <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      {!isPlayer && <Footer />}
+      <ScrollToTop />
     </div>
   );
 }
-
-
-
-
