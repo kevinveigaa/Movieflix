b4164dae618caa3d04c350179ebea1f096169530
kevinveigaa <92695662+kevinveigaa@users.noreply.github.com>
2026-09-22 16:07:02 -0300

    revert: keep mobile app unchanged

--- android/app/src/main/java/com/movieflix/app/PlayerActivity.java
+++ android/app/src/main/java/com/movieflix/app/PlayerActivity.java
@@ -4,10 +4,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.KeyEvent;
-import android.view.Window;
-import android.view.WindowManager;
-import android.media.AudioManager;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -51,7 +47,6 @@ public class PlayerActivity extends AppCompatActivity {
     private String titleId;
     private boolean isSeries;
     private List<String> episodes = new ArrayList<>();
-    private boolean fullscreen = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -94,9 +89,6 @@ public class PlayerActivity extends AppCompatActivity {
                         .setDataSourceFactory(dataSourceFactory))
                 .build();
 
-        playerView.setFocusable(true);
-        playerView.setFocusableInTouchMode(true);
-        playerView.requestFocus();
         playerView.setPlayer(player);
         playerView.setControllerShowTimeoutMs(3000);
         playerView.setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING);
@@ -130,66 +122,6 @@ public class PlayerActivity extends AppCompatActivity {
         long pos = lerPosicao();
         if (pos > 0) player.seekTo(pos);
         player.setPlayWhenReady(true);
-    }
-
-    /** Controles nativos do player para setas/OK/menus de controles TV. */
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        if (event.getAction() != KeyEvent.ACTION_DOWN || player == null) {
-            return super.dispatchKeyEvent(event);
-        }
-        switch (event.getKeyCode()) {
-            case KeyEvent.KEYCODE_DPAD_LEFT:
-                player.seekBack();
-                playerView.showController();
-                return true;
-            case KeyEvent.KEYCODE_DPAD_RIGHT:
-                player.seekForward();
-                playerView.showController();
-                return true;
-            case KeyEvent.KEYCODE_DPAD_CENTER:
-            case KeyEvent.KEYCODE_ENTER:
-            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-                if (player.isPlaying()) player.pause(); else player.play();
-                playerView.showController();
-                return true;
-            case KeyEvent.KEYCODE_DPAD_UP:
-                ((AudioManager) getSystemService(AUDIO_SERVICE)).adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-                return true;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
-                ((AudioManager) getSystemService(AUDIO_SERVICE)).adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-                return true;
-            case KeyEvent.KEYCODE_MENU:
-                alternarFullscreen();
-                return true;
-            case KeyEvent.KEYCODE_BACK:
-                if (fullscreen) {
-                    alternarFullscreen();
-                    return true;
-                }
-                break;
-            default:
-                break;
-        }
-        return super.dispatchKeyEvent(event);
-    }
-
-    private void alternarFullscreen() {
-        fullscreen = !fullscreen;
-        if (fullscreen) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        } else {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
-        playerView.showController();
     }
 
     private String chavePosicao() {
